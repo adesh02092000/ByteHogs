076e70f3af07c8b55d9f5c8bfb78717ba93f747b
Ankur Anant <ankuranant99@gmail.com>
2019-07-14 12:42:21 +0530

    code fix

--- Assets/Scripts/ModelInit.cs
+++ Assets/Scripts/ModelInit.cs
@@ -6,10 +6,42 @@ public class ModelInit : MonoBehaviour
 {
     private string modelSelected;
     private GameObject modelPrefab;
+    private GameObject imageTracker;
+
+    // prefabs
+    public GameObject Nuike;
+    public GameObject Whuietie;
+    public GameObject Sepoeikeis;
+    public GameObject BBS;
     private void Awake()
     {
         modelSelected = PlayerPrefs.GetString("SelModel");
-        modelPrefab = GameObject.FindGameObjectWithTag(modelSelected);
-        Instantiate(modelPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        imageTracker = GameObject.Find("ImageTarget");
+        switch(modelSelected) {
+            case "Nuike":
+                modelPrefab = Instantiate(Nuike, new Vector3(0, 0, 0), Quaternion.identity);
+                modelPrefab.transform.parent = imageTracker.transform;
+                break;
+
+            case "Whuietie":
+                modelPrefab = Instantiate(Whuietie, new Vector3(0, 0, 0), Quaternion.identity);
+                modelPrefab.transform.parent = imageTracker.transform;
+                break;
+
+            case "Sepoeikeis":
+                modelPrefab = Instantiate(Sepoeikeis, new Vector3(0, 0, 0), Quaternion.identity);
+                modelPrefab.transform.parent = imageTracker.transform;
+                break;
+
+            case "BBS":
+                modelPrefab = Instantiate(BBS, new Vector3(0, 0, 0), Quaternion.identity);
+                modelPrefab.transform.parent = imageTracker.transform;
+                break;
+
+            default:
+                Debug.Log("yolo");
+                break;
+
+        }
     }
 }
